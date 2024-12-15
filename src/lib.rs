418a200d36c9297ee0f24f5c104d5eec931a6ff9
Gina Sohn <gina7484@gmail.com>
2024-12-15 21:36:04 +0000

    submodule DAM

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,26 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn create_context() -> PyResult<String> {
+    // Create a context and return it
+    todo!()
+}
+
+#[pyfunction]
+fn add_node(// PyResult<String> // Context
+// Information for the node to add
+) -> PyResult<String> {
+    todo!()
+}
+
+#[pyfunction]
+fn run_simulation(// PyResult<String> // Context
+    // features for running the simulation
+) -> PyResult<String> {
+    todo!()
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyo3_step(m: &Bound<'_, PyModule>) -> PyResult<()> {
